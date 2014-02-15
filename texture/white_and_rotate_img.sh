fbd595173eeb3395b8647552cd477799bf9849b9
Satoshi MITANI <mitans02@ybb.ne.jp>
2014-02-16 08:00:39 +0900

    scale

--- texture/white_and_rotate_img.sh
+++ texture/white_and_rotate_img.sh
@@ -6,10 +6,9 @@ if [ "$1" = "" -o "$2" = "" ]; then
   exit 1
 fi
 
-n=0
 for f in `ls $1`
 do
-  convert $1/$f \( +clone -alpha opaque -fill white -colorize 100% \) +swap \
-    -compose Over -composite -alpha off -rotate 270 $2/$n.png
-  n=`expr $n + 1`
+#  convert $1/$f \( +clone -alpha opaque -fill white -colorize 100% \) +swap \
+#   -compose Over -composite -alpha off -rotate 270 $2/$n.png
+   convert $1/$f -crop "100x100+0+0" -rotate 270 -flop -geometry 128x128 png32:$2/$f
 done
