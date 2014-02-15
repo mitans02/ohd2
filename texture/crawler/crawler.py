ab7abb128bc198980e2e68dac9ed585cfec5d810
Satoshi MITANI <mitans02@ybb.ne.jp>
2014-02-15 21:18:18 +0900

    reforctored

--- texture/crawler/crawler.py
+++ texture/crawler/crawler.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python
+# coding: UTF-8
 import httplib
 import re
+import pprint
+
+# borrow from http://tagnoheya.com/charlist/charlist2.html
+chars = list(u"ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ")
 
 def fetch(file, url):
 	c = httplib.HTTPConnection("www.fileformat.info")
@@ -11,18 +17,61 @@ def fetch(file, url):
 	outfile.write(data)
 	outfile.close()
 
+def tohex(s):
+	res = ""
+	for c in s:
+		res += "%02X" % ord(c)
+	return res
 
-conn = httplib.HTTPConnection("www.fileformat.info")
-conn.request("GET", "/info/unicode/block/hiragana/images.htm")
+def find_char_num(hexc):
+	i = 0
+	for c in chars:
+		if tohex(c).lower() == hexc:
+			return i
+		i = i + 1
 
-res = conn.getresponse()
-html = res.read().decode("UTF-8")
+def crawl_char_image(host, path):
+	extracted = {}
+	conn = httplib.HTTPConnection(host)
+	conn.request("GET", path)
 
-r = re.compile(r"src=\"(.+/char\/(.+)\/.+?\.png)")
+	res = conn.getresponse()
+	html = res.read().decode("UTF-8")
+	conn.close()
 
-conn.close()
+	r = re.compile(r"src=\"(.+/char\/(.+)\/.+?\.png)")
 
-for mat in r.finditer(html):
-	print 'save/' + mat.group(2).rstrip() + '.png'
-	fetch('save/' + mat.group(2).rstrip() + '.png', mat.group(1))
+	for mat in r.finditer(html):
+		uhex = mat.group(2).rstrip().lower()
 
+		extracted[uhex] = mat.group(1)
+
+	return extracted
+
+
+hexchars = {};
+for c in chars:
+	hexchars[tohex(c).lower()] = 0
+
+hiraganas = crawl_char_image("www.fileformat.info", "/info/unicode/block/hiragana/images.htm")
+katakanas = crawl_char_image("www.fileformat.info", "/info/unicode/block/katakana/images.htm")
+
+allchars = dict(hiraganas, **katakanas)
+
+pprint.pprint(allchars)
+
+for uhex in allchars.keys():
+	if hexchars.has_key(uhex) == False :
+		print uhex + " is ignored"
+		continue
+
+	num = str(find_char_num(uhex))
+
+	print 'save/' + num + '.png'
+	fetch('save/' + num + '.png', allchars[uhex])
+	hexchars[uhex] = 1
+
+
+for k in hexchars.keys():
+	if hexchars[k] == 0:
+		print "ERROR: "+k+" is not downloaded"
