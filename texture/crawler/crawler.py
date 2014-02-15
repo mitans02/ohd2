7aede94790bf0ac34885381bdbbc9aa45dc12a43
Satoshi MITANI <mitans02@ybb.ne.jp>
2014-02-15 18:53:42 +0900

    added char image crawler

---
+++ texture/crawler/crawler.py
@@ -0,0 +1,28 @@
+import httplib
+import re
+
+def fetch(file, url):
+	c = httplib.HTTPConnection("www.fileformat.info")
+	c.request("GET", url)
+
+	data = c.getresponse().read()
+
+	outfile = open(file, 'wb')
+	outfile.write(data)
+	outfile.close()
+
+
+conn = httplib.HTTPConnection("www.fileformat.info")
+conn.request("GET", "/info/unicode/block/hiragana/images.htm")
+
+res = conn.getresponse()
+html = res.read().decode("UTF-8")
+
+r = re.compile(r"src=\"(.+/char\/(.+)\/.+?\.png)")
+
+conn.close()
+
+for mat in r.finditer(html):
+	print 'png/' + mat.group(2).rstrip() + '.png'
+	fetch('png/' + mat.group(2).rstrip() + '.png', mat.group(1))
+
